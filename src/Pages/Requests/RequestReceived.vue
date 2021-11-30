8ed7b376aa4b48a825de008a411620e1ee2506b6
Joaquin del VAL <delvaljoaquin@gmail.com>
2021-11-30 18:38:19 -0300

    ahora podemos ver los requests received

--- src/Pages/Requests/RequestReceived.vue
+++ src/Pages/Requests/RequestReceived.vue
@@ -1,13 +1,54 @@
 <template>
-<h2>Request Received</h2>
+  <section>
+    <base-card>
+      <header>
+        <h2>REQUESTS RECEIVED</h2>
+      </header>
+      <ul v-if='hasRequests'>
+        <requests-item v-for='req in receivedReq'
+                      :key='req.id'
+                      :email='req.userEmail'
+                      :message='req.message'
+        >
+        </requests-item>
+      </ul>
+      <h3 v-else>You dont have any request yet!</h3>
+    </base-card>
+  </section>
 </template>
 
 <script>
+import RequestsItem from '../../components/requests/RequestsItem.vue';
+
 export default {
-  name: 'RequestReceived'
+
+  name: 'RequestReceived',
+  components: RequestsItem,
+  computed: {
+    receivedReq() {
+      return this.$store.getters['requests/requests'];
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests'];
+    }
+
+  }
 };
 </script>
 
 <style scoped>
+header {
+  text-align: center;
+}
 
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import store from '@/store/index.js';
 import BaseCard from '@/components/ui/BaseCard';
 import BaseButton from '@/components/ui/BaseButton';
 import BaseBadge from './components/ui/BaseBadge.vue';
+import RequestsItem from '@/components/requests/RequestsItem';
 
 
 const app = createApp(App)
@@ -18,5 +19,7 @@ app.use(store);
 app.component('base-card' ,BaseCard);
 app.component('base-btn' ,BaseButton);
 app.component('base-badge' ,BaseBadge);
+
+app.component('requests-item', RequestsItem);
 
 app.mount('#app');
