7d52bf01f4fe7b8fa4de0a9f5fc482e793690027
Joaquin del VAL <delvaljoaquin@gmail.com>
2021-12-01 12:22:47 -0300

    agrego axios, y methodo PUT para guardar discos en firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue';
 
-import  App  from './App.vue';
+import App from './App.vue';
 
 import router from '@/routerr';
 import store from '@/store/index.js';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 import BaseCard from '@/components/ui/BaseCard';
 import BaseButton from '@/components/ui/BaseButton';
@@ -11,14 +13,15 @@ import BaseBadge from './components/ui/BaseBadge.vue';
 import RequestsItem from '@/components/requests/RequestsItem';
 
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(router);
 app.use(store);
+app.use(VueAxios, axios);
 
-app.component('base-card' ,BaseCard);
-app.component('base-btn' ,BaseButton);
-app.component('base-badge' ,BaseBadge);
+app.component('base-card', BaseCard);
+app.component('base-btn', BaseButton);
+app.component('base-badge', BaseBadge);
 
 app.component('requests-item', RequestsItem);
 
--- src/store/modules/records/actions.js
+++ src/store/modules/records/actions.js
@@ -1,8 +1,11 @@
 export default {
-  registerRecord(context, data) {
+
+
+  async registerRecord(context, data) {
+    const userId = context.rootGetters.getUserId;
     const recordsData = {
       // id: newDate().toISOString(),
-      id: context.rootGetters.getUserId,
+      // id: context.rootGetters.getUserId,
       // id: 'r3',
       name: data.name,
       band: data.bandName,
@@ -11,7 +14,29 @@ export default {
       description: data.desc
     };
 
-    context.commit('registerRecord', recordsData);
+    // HTTP REQ USING FETCH
+
+    // const response = await fetch(`https://recordsapp-e4425-default-rtdb.firebaseio.com/records/${userId}.json`,
+    //   {
+    //     method: 'PUT',
+    //     body: JSON.stringify(recordsData)
+    //   });
+    //
+    // if (!response.ok) {
+    //   // error
+    // }
+
+    // USING AXIOS
+    const axios = require('axios').default;
+    axios
+      .put(`https://recordsapp-e4425-default-rtdb.firebaseio.com/records/${userId}.json`, JSON.stringify(recordsData))
+      .then(response => (this.info = response))
+
+
+    context.commit('registerRecord', {
+      ...recordsData,
+      id: userId
+    });
 
   }
 };
