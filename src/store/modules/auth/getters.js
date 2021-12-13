7cdf9398a8c9059444465407e14b10ad12c8946f
Joaquin del VAL <delvaljoaquin@gmail.com>
2021-12-13 12:08:00 -0300

    agrego el token a las requests para poder realizarlas correctamente

--- src/store/modules/auth/getters.js
+++ src/store/modules/auth/getters.js
@@ -1,5 +1,10 @@
 export default {
   getUserId(state) {
     return state.userId;
+  },
+  // para utilizar el token en partes de la pagina donde sea necesario debido a las reglas del backend (estar logeado etc) , creo este getter
+  // para que pueda llamar al token desde cualquier otro module de vuex.
+  token(state){
+    return state.token;
   }
 }
